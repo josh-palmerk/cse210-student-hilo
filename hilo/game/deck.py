0db0c77304cf749a3282f44ddb4b987bcd21a531
Cameron <axxes9mc@gmail.com>
2021-10-08 11:18:04 -0600

    fixed potential bug with num_cards

--- hilo/game/deck.py
+++ hilo/game/deck.py
@@ -18,4 +18,5 @@ class Deck:
         if self.num_cards == 52:
             print("Shuffling deck...")
             self.deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13]
+            self.num_cards = 0
         return self.next_card
